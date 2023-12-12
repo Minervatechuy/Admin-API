309cac86f64054820b4c98aea2d3cf627afa2557
Maximiliano Gonzalez <maximiliano.gonzalez@sabre.com>
2023-12-12 01:43:05 -0300

    logic

--- API/Docker-Webserver/flaskServer/webserver/static/js/portada.js
+++ API/Docker-Webserver/flaskServer/webserver/static/js/portada.js
@@ -28,7 +28,7 @@ function APIrequest(sig_pos) {
     if (XMLHttpRequest) {
         var xhr = new XMLHttpRequest();
         xhr.open('GET', apiurl, true);
-        xhr.withCredentials = true; // Enable credentials (cookies, authorization headers, etc.)
+        xhr.withCredentials = true;
     
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4 && xhr.status == 200) {
