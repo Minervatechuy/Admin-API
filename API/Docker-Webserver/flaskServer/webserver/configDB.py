76f3f1dc900f41161b5e8de58689c6444d28cfb3
Minervatechuy <144599132+Minervatechuy@users.noreply.github.com>
2023-12-13 18:12:31 -0300

    Update configDB.py

--- API/Docker-Webserver/flaskServer/webserver/configDB.py
+++ API/Docker-Webserver/flaskServer/webserver/configDB.py
@@ -3,7 +3,7 @@ import mysql.connector
 def connect():
     hostname = 'prod-db.cyimwtghazuk.us-east-1.rds.amazonaws.com'
     username = 'minervatech'
-    password = 'minervatech'
+    password = 'MinervallTech'
     database = 'minervatech'
 
     return mysql.connector.connect( host=hostname, user=username, passwd=password, db=database)
